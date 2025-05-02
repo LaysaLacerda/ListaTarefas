ac387514399a68458192c6731b1fea4f4f4b886c
LaysaLacerda <105090413+LaysaLacerda@users.noreply.github.com>
2025-05-02 10:45:12 -0300

    Crud de tarefas
    Listagem com ou sem consulta, criar, alterar, deletar ou completar uma tarefas

--- src/middlewares/routes.js
+++ src/middlewares/routes.js
@@ -7,53 +7,87 @@ const database = new Database
 export const routes = [
     {
         method: 'GET',
-        url: buildRoutePath('/users'),
+        url: buildRoutePath('/tasks'),
         handler: (req, res) => {
             const { search } = req.query 
             
-            const users = database.select('users', search ? { 
-                name: search,
-                email: search,
+            const tasks = database.select('tasks', search ? { 
+                title: search,
+                description: search,
             }: null)
 
-            return res.end(JSON.stringify(users))   
+            return res.end(JSON.stringify(tasks))   
         }
     },
     {
         method: 'POST',
-        url: buildRoutePath('/users'),
+        url: buildRoutePath('/tasks'),
         handler: (req, res) => {
-            const {name, email} = req.body
+            const {title, description} = req.body
 
-            const user = ({
+            const task = ({
                 id: randomUUID(),
-                name,
-                email,
+                title,
+                description,
+                completed_at: null,
+                created_at: new Date(),
+                updated_at:null,
             })
 
-            database.insert('users', user)
+            database.insert('tasks', task)
             return res.writeHead(201).end()
         }
     },
     {
         method: 'DELETE',
-        url:  buildRoutePath('/users/:id'), 
+        url:  buildRoutePath('/tasks/:id'), 
         handler: (req, res) => {
             const { id }= req.params
 
-            database.delete('users',id)
+            database.delete('tasks',id)
 
             return res.writeHead(204).end()
         }
     },
     {
         method: 'PUT',
-        url:  buildRoutePath('/users/:id'),         
+        url:  buildRoutePath('/tasks/:id'),         
         handler: (req, res) => {
             const { id }= req.params
-            const { name, email } = req.body
+            const { title, description } = req.body
+            const updated_at = new Date()
+            const completed_at = null
+            let created_at = null
 
-            database.update('users',id,{name,email,})
+            const task = database.select('tasks', { 
+                id: id,
+            })
+
+            if (task) {
+                created_at = task[0].created_at
+            }
+            
+            database.update('tasks',id,{title,description,completed_at,created_at,updated_at})    
+
+            return res.writeHead(204).end()
+        }
+    },
+    {
+        method: 'PATCH',
+        url:  buildRoutePath('/tasks/:id/complete'),         
+        handler: (req, res) => {
+            const { id } = req.params
+            const completed_at = new Date()
+
+            const task = database.select('tasks', { 
+                id: id,
+            })
+
+            if (task) {
+                const { title, description, created_at, updated_at } = task[0]
+
+                database.update('tasks',id,{title,description,completed_at,created_at,updated_at})    
+            }
 
             return res.writeHead(204).end()
         }
